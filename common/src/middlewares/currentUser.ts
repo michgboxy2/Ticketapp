0852f9fc38ca1e03e54ffde056f29ac089777ccb
michgboxy <michgboxy@gmail.com>
2020-07-18 16:02:47 +0100

    commonly shared code among different microservices uploaded to npm registry

---
+++ common/src/middlewares/currentUser.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+//make req take the currentUser object
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+
+    req.currentUser = payload;
+    // next();
+  } catch (err) {}
+
+  next();
+};
