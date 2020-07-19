7ed2d6f1d76ad51b816117f4c6de13088bbfb03f
michgboxy <michgboxy@gmail.com>
2020-07-19 11:19:00 +0100

    ticket service infra setup

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,57 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfasdf";
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email,
+      password,
+    })
+    .expect(201)
+    .then((data) => {
+      const cookie = data.get("Set-Cookie");
+      console.log(cookie);
+      // done();
+      return cookie;
+    });
+};
