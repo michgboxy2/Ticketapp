9bd41ed23058f2575e9b0225d3a52a5e60fad1b9
michgboxy <michgboxy@gmail.com>
2020-07-20 02:42:25 +0100

    update to ingress infra file for ticket service entry. error handler updates and tests setup modification for dynamic user cookie creation on each request

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -41,7 +41,7 @@ afterAll(async () => {
 global.signin = () => {
   //Build a JWT payload. {id, email}
   const payload = {
-    id: "12345",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
