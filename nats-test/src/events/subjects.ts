585269ec966a6d8d2b6f264f0c2d7549e0c73728
michgboxy <michgboxy@gmail.com>
2020-07-23 00:19:33 +0100

    nats streaming server setup

---
+++ nats-test/src/events/subjects.ts
@@ -0,0 +1,7 @@
+export enum Subjects {
+  TicketCreated = "ticket:created",
+  TicketUpdated = "ticket:updated",
+  OrderUpdated = "order:updated",
+}
+
+const printSubject = (subject: Subjects) => {};
