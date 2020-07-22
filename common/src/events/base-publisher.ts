6285f06864efebe1eb106dd87062e825b1eaf472
michgboxy <michgboxy@gmail.com>
2020-07-23 00:12:58 +0100

    add NAT setup to common files

---
+++ common/src/events/base-publisher.ts
@@ -0,0 +1,29 @@
+import { Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+  //This is a generic class because of the <> //abstract classes can only be extended. the can't be new()
+  abstract subject: T["subject"];
+  private client: Stan;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  publish(data: T["data"]): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.client.publish(this.subject, JSON.stringify(data), (err) => {
+        if (err) {
+          return reject(err);
+        }
+        console.log("Event published to subject", this.subject);
+        resolve();
+      });
+    });
+  }
+}
