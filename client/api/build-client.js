170a94347dce050cf45eb8e4f8f4f4c799cc0205
michgboxy <michgboxy@gmail.com>
2020-09-10 02:30:03 +0100

    revert client back to the dev url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,8 +6,8 @@ export default ({
   if (typeof window === "undefined") {
     //we are on ther server
     return axios.create({
-      // baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-      baseURL: 'http://www.aldra.com.ng',
+      baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      // baseURL: 'http://www.aldra.com.ng',
       headers: req.headers,
     });
   } else {
